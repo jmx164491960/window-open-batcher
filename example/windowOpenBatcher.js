834f4b8c2aada120132e34eb6a2dd622398e1c4c
jiangminxi <jiangminxi@banggood.com>
2019-12-23 17:30:04 +0800

    调试完成

--- example/windowOpenBatcher.js
+++ example/windowOpenBatcher.js
@@ -1,4 +1,25 @@
 import WindowOpenBatcher from '../lib/index';
+import {
+  MessageBox
+} from 'element-ui';
+import 'element-ui/lib/theme-chalk/message-box.css';
+import intercept_icon from './assets/intercept_icon.png';
+import './demo.css';
 
-const windowOpenBatcher = new WindowOpenBatcher();
+const windowOpenBatcher = new WindowOpenBatcher({
+  callback: () => {
+    MessageBox.alert(
+      `由于谷歌系统对批量打开功能的拦截，请点击上方的“<img src=${intercept_icon}>”，之后勾选第一个选项：始终允许${window.location.origin}显示弹出式窗口和重定向，并点击“完成”。`,
+      '', 
+      {
+        customClass: "g-intercept-operate-tip",
+        dangerouslyUseHTMLString: true,
+        confirmButtonText: '完成',
+        callback: action => {
+          // this.isIntercept = false
+        }
+      }
+    )
+  }
+});
 export default windowOpenBatcher;
--- lib/index.ts
+++ lib/index.ts
@@ -1,46 +1,79 @@
 interface storageDataParams {
   count: number,
   hrefArr: Array<string>
-  enable: Boolean
+  time: number
 }
 
 interface constructorParams {
   seconds?: number
   storageKey?: string
+  callback?: Function
 }
 
 export default class OpenWindowBatcher {
   storageKey: string
   seconds: number
+  callback: Function;
   
   constructor (params: constructorParams) {
     if (params === undefined) params = {}
     this.storageKey = params.storageKey || 'OpenWindowBatcher'
-    this.seconds = params.seconds || 1000;
+    this.seconds = params.seconds * 1000 || 1000;
+    this.callback = params.callback || null;
   }
   check() {
     const storageData = this.getStorageData();
-    if (!storageData.enable) {
+    // 超时了不运行
+    if (this.isTimeout(storageData.time)) {
       return;
     }
 
     // 如果打开的页面在列表内
-    const match = storageData.hrefArr.some(href => {
-      return window.location.href.includes(href);
-    });
+    const match = this.isMatchHref(storageData.hrefArr);
     if (match) {
       storageData.count ++;
       this.setStorageData(storageData);
     }
 
+    const isTimeout = this.isTimeout(storageData.time);
     window.setTimeout(() => {
       const storageData = this.getStorageData();
-      if (storageData.enable && storageData.count < 2) {
+      // 满足以下条件是就是被浏览器拦截了的情况
+      console.log(storageData.count < 2);
+      console.log(!isTimeout);
+      console.log(this.isMatchHref(storageData.hrefArr));
+      debugger
+      if (storageData.count < 2
+        && !isTimeout
+        && this.isMatchHref(storageData.hrefArr)
+      ) {
+        // 重置
         this.setStorageData(this.getNewStroageData());
+        // 关闭页面
         window.close();
       }
     }, this.seconds);
   }
+  /**
+   * 当前href是否命中
+   * @param hrefArr 
+   */
+  isMatchHref(hrefArr: Array<string>): Boolean {
+    return hrefArr.some(href => {
+      return window.location.href.includes(href);
+    });
+  }
+  /**
+   * 是否超时
+   * @param time 
+   */
+  isTimeout(time: number): Boolean {
+    console.log(new Date().getTime(), Number(time));
+    return new Date().getTime() - Number(time) > this.seconds;
+  }
+  /**
+   * 获取当前localstroage对应命名空间
+   */
   getStorageData(): storageDataParams {
     let storageData;
     try {
@@ -50,34 +83,33 @@ export default class OpenWindowBatcher {
     }
     return storageData;
   }
+  /**
+   * 获取一个初始化的对象
+   */
   getNewStroageData(): storageDataParams {
     return {
       count: 0,
       hrefArr: [],
-      enable: false
+      time: new Date().getTime()
     };
   }
+  /**
+   * 设置localstorage
+   * @param obj 
+   */
   setStorageData(obj: storageDataParams) {
     localStorage.setItem(this.storageKey, JSON.stringify(obj));
   }
-  // setCount(value) {
-  //   const storageData = this.getStorageData();
-  //   storageData['count'] = value;
-  //   localStorage.setItem(this.storageKey, JSON.stringify(storageData));
-  // }
-  getCount() {
-    const storageData = this.getStorageData();
-    if (!storageData['count']) {
-      return 0;
-    } else {
-      return Number(storageData['count']);
-    }
-  }
-  open(hrefArr, callback) {
+  /**
+   * 主方法
+   * @param hrefArr 需要批量打开的链接数组
+   * @param callback 回调函数
+   */
+  open(hrefArr: Array<string>, callback?: Function) {
     const obj = {
       count: 0,
       hrefArr,
-      enable: true
+      time: new Date().getTime()
     };
     this.setStorageData(obj);
     hrefArr.forEach((str) => {
@@ -85,19 +117,14 @@ export default class OpenWindowBatcher {
     })
 
     let timer = setInterval(() => {
-      const count = this.getCount();
+      const count = this.getStorageData()['count'];
       // 若出于被拦截状态
       if (count < 2) {
-        // this.isIntercept = true
-        // MessageBox.alert(parcelManagementLang('stopIntercept1') + `<img src=${intercept_icon}>` + parcelManagementLang('stopIntercept2') + window.location.origin + parcelManagementLang('stopIntercept3'), '', {
-        //   customClass: "g-intercept-operate-tip",
-        //   dangerouslyUseHTMLString: true,
-        //   confirmButtonText: window.$lang('common')('done'),
-        //   callback: action => {
-        //   }
-        // })
-
-        callback();
+        if (this.callback) {
+          this.callback();
+        } else if (callback) {
+          callback()
+        }
         clearInterval(timer)
       } else {
         clearInterval(timer)
